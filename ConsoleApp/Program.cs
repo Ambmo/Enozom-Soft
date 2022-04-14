22cb391b2779e8d3229a9c76979de64fdb8859a1
Ahmed Mostafa <77973041+Ambmo@users.noreply.github.com>
2022-04-14 16:43:12 +0200

    Trial"Empty"

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
